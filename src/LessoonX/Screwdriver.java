1a253670d7596e98a2c6c14c06868714ca50e14c
YoungPadavan <ggg1786@gmail.com>
2018-04-02 23:49:32 +0300

    commit

--- src/LessoonX/Screwdriver.java
+++
@@ -1,7 +0,0 @@
-package lesson8;
-
-public class Screwdriver {
-    public void cut(){
-        System.out.println("Бить");
-    }
-}
--- src/LessoonX/Start.java
+++
@@ -1,9 +0,0 @@
-package lesson8;
-
-public class Start {
-    public static void main(String[] args) {
-    Drill drill = new Drill();
-
-    drill.cut(drill);
-        }
-}
