30fb4cc7ced08a7e2b16696d51886e667c208b19
YoungPadavan <ggg1786@gmail.com>
2018-04-12 21:11:23 +0300

    1

--- src/Lesson9/Start.java
+++ src/Lesson9/Start.java
@@ -1,32 +1,51 @@
 package Lesson9;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Start {
-    public static void main(String[] args) throws FileNotFoundException {
-        try {
-            FileReader reader = new FileReader("C:\\java\\1\\Рассказ.txt");
-            BufferedReader bf = new BufferedReader(reader);
-            String s;
-            while ((s = bf.readLine()) != null) {
-                System.out.println(s);
+
+        public static void main(String[] args) throws FileNotFoundException {
+            getWordCount("C:\\java\\1\\Рассказ.txt");
+        }
+
+        private static void getWordCount(String filename) throws FileNotFoundException {
+            File file = new File("C:\\java\\1\\Рассказ.txt");
+            Scanner scanner = new Scanner(file);
+            int words = 0;
+
+            while (scanner.hasNextLine()) {
+                String[] array = scanner.nextLine().split(" ");
+                words = words + array.length;
             }
-            bf.close();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-
-        File file = new File("C:\\java\\1\\Рассказ.txt");
-        Scanner scanner = new Scanner(file);
-        int words = 0;
-            String[] array = scanner.nextLine().split(" ");
-            words = words + array.length;
             System.out.println("Количество слов " + words);
+
+       
+                String text = scanner.nextLine();
+                HashMap<String,Integer> wordsCount = new HashMap<>();
+            StringTokenizer tokenizer = new StringTokenizer(text, ",.:;!?\t");
+            while (tokenizer.hasMoreElements()) {
+                String token = tokenizer.nextToken();
+                Integer value = wordsCount.get(token);
+                if (value != null)
+                    wordsCount.put(token, value + 1);
+                else
+                    wordsCount.put(token, 1);
+            }
+            System.out.println("\ncount: %d\n"+ wordsCount.size());
+            for(Map.Entry<String,Integer> entry : wordsCount.entrySet())
+                System.out.println("%s -> %d\n"+ entry.getKey()+ entry.getValue());
             scanner.close();
-        }
-    }
+            }
+}
+
+
+
+
 
 
 
